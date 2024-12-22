18bcd91354ef0b6ad54678d53134942704407018
AtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-12-22 19:37:24 +0100

    Add floating buttons to create list and notes.

--- lib/ui/app_theme.dart
+++ lib/ui/app_theme.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+  scaffoldBackgroundColor: Colors.white,
 );
--- lib/ui/views/main_view.dart
+++ lib/ui/views/main_view.dart
@@ -1,70 +1,48 @@
 import 'package:flutter/material.dart';
 
-class MainView extends StatefulWidget {
+class MainView extends StatelessWidget {
   const MainView({
     super.key,
   });
 
   @override
-  State<MainView> createState() => _MainViewState();
-}
-
-class _MainViewState extends State<MainView> {
-  int count = 0;
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              print("Create list");
+            },
+            foregroundColor: Colors.grey[50],
+            backgroundColor: Colors.blue,
+            shape: CircleBorder(),
+            mini: true,
+            child: Icon(
+              Icons.check_box_outlined,
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              print("Create note");
+            },
+            foregroundColor: Colors.grey[50],
+            backgroundColor: Colors.blue,
+            shape: CircleBorder(),
+            mini: true,
+            child: Icon(
+              Icons.create_outlined,
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           spacing: 4.0,
           children: [
-            Text('Count: $count'),
-            Row(
-              spacing: 10.0,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                OutlinedButton(
-                  onPressed: () {
-                    setState(() {
-                      count = 0;
-                    });
-                  },
-                  child: Text("Reset"),
-                ),
-                FilledButton(
-                  onPressed: () {
-                    setState(() {
-                      count++;
-                    });
-                  },
-                  child: const Text("Click me"),
-                ),
-              ],
-            ),
-            GestureDetector(
-              onLongPress: () {
-                setState(() {
-                  count += 50;
-                });
-              },
-              onVerticalDragUpdate: (details) {
-                setState(() {
-                  count++;
-                });
-              },
-              child: SizedBox(
-                width: 256.0,
-                height: 256.0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                  ),
-                  alignment: Alignment.center,
-                  child: Text("Long press me to add 50."),
-                ),
-              ),
-            ),
+            Text('Je suis une application de notes.'),
           ],
         ),
       ),
